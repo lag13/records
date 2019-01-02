6578fa924ba782ff3bcb5fd1a3fd6831766036d6
Lucas Groenendaal <groenendaal92@gmail.com>
2019-01-02 08:14:49 +0000

    Make cmdline print errors about input files
    Woo!

--- internal/parsefile/parsefile_test.go
+++ internal/parsefile/parsefile_test.go
@@ -1,8 +1,10 @@
 package parsefile_test
 
 import (
+	"errors"
 	"os"
 	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/lag13/records/internal/parsefile"
@@ -31,6 +33,37 @@ func TestParseFile(t *testing.T) {
 				OpenErr: os.ErrPermission,
 			},
 			wantParseErr: "badperm/file.txt: do not have permission to open this file",
+		},
+		{
+			name: "unknown error",
+			file: parsefile.File{
+				Name:    "itsa/mystery.txt",
+				Content: nil,
+				OpenErr: errors.New("unknown error"),
+			},
+			wantParseErr: "itsa/mystery.txt: encountered an unknown error when opening this file: unknown error",
+		},
+		{
+			name: "unknown open error",
+			file: parsefile.File{
+				Name:    "itsa/mystery.txt",
+				Content: nil,
+				OpenErr: errors.New("unknown error"),
+			},
+			wantParseErr: "itsa/mystery.txt: encountered an unknown error when opening this file: unknown error",
+		},
+		{
+			name: "so many problems with the file",
+			file: parsefile.File{
+				Name: "hey.txt",
+				Content: strings.NewReader(`hey|there,buddy !
+noseps
+this,is,an,okay,line
+too|few|seps`),
+			},
+			wantParseErr: `hey.txt:1: there should only be one type of separator in a single line but multiple separators ('|', ',', ' ') were specified
+hey.txt:2: there is only one field in the record but there should be 5
+hey.txt:4: there were only 3 fields when there should have been 5`,
 		},
 	}
 	for _, test := range tests {
