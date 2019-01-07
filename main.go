bb892c0a7c0571381edadab35f8e9bf792ffcdf7
Lucas Groenendaal <groenendaal92@gmail.com>
2019-01-07 08:44:48 +0000

    Print out info about semantically invalid data

--- main.go
+++ main.go
@@ -6,23 +6,43 @@ import (
 	"strings"
 
 	"github.com/lag13/records/internal/parsefile"
+	"github.com/lag13/records/internal/person"
 )
 
 func main() {
 	parseErrs := []string{}
-	{ // parse each file
+	// parse each file. TODO: This is getting QUITE messy and
+	// feels a little too complicated for being in main. I'm
+	// thinking that I should break this up into separate
+	// structural parsing and semantic parsing portions. Or
+	// perhaps I'll try to move the code around a little (maybe
+	// make a new unit that can be unit tested?). But! tests pass
+	// so at least we can refactor with a little confidence.
+	{
 		for _, fileName := range os.Args[1:] {
 			fh, err := os.Open(fileName)
-			_, parseErr := parsefile.ParseFile(parsefile.File{
+			defer func(fh *os.File) {
+				if fh == nil {
+					return
+				}
+				_ = fh.Close()
+			}(fh)
+			lines, parseErr := parsefile.ParseFile(parsefile.File{
 				Name:    fileName,
 				Content: fh,
 				OpenErr: err,
 			})
 			if parseErr != "" {
 				parseErrs = append(parseErrs, parseErr)
+				continue
 			}
-			if fh != nil {
-				_ = fh.Close()
+			for i, line := range lines {
+				_, semParseErrs := person.Parse(line)
+				annotatedSemParseErrs := []string{}
+				for _, semParseErr := range semParseErrs {
+					annotatedSemParseErrs = append(annotatedSemParseErrs, fmt.Sprintf("%s:%d: %s", fileName, i+1, semParseErr))
+				}
+				parseErrs = append(parseErrs, annotatedSemParseErrs...)
 			}
 		}
 	}
