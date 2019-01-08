958a3967ff5138af20eb4bae9a11bcb1daa94a7a
Lucas Groenendaal <groenendaal92@gmail.com>
2019-01-08 07:07:41 +0000

    Finish sorting by gender feature
    Next we'll tackle the other kinds of sorts that have been requested.
    I'll probably also make a flag to switch between which type of sort
    should be performed.

--- internal/person/person.go
+++ internal/person/person.go
@@ -3,6 +3,8 @@ package person
 
 import (
 	"fmt"
+	"sort"
+	"strings"
 	"time"
 )
 
@@ -17,8 +19,20 @@ type Person struct {
 
 // Parse converts a list of fields into a Person struct.
 func Parse(fields []string) (Person, []string) {
+	// TODO: This constant 5 now lives in 2 places which seems not
+	// good. I think it should be passed into this package and the
+	// parsefile package. That would mean making these 2 packages
+	// more general.
 	const needNumFields = 5
 	if l := len(fields); l != needNumFields {
+		// TODO: I think this should be a panic instead of a
+		// parse error thingy (we could even just let this
+		// function panic naturally). Because there is NEVER a
+		// reason this function should be passed a slice that
+		// has a length less than 5. If that ever happens it
+		// is clearly the callers fault so why should this
+		// function worry about checking for a condition which
+		// should never happen?
 		return Person{}, []string{fmt.Sprintf("fields slice has length %d but must be exactly length %d", l, needNumFields)}
 	}
 	parseErrs := []string{}
@@ -39,4 +53,21 @@ func Parse(fields []string) (Person, []string) {
 		return Person{}, parseErrs
 	}
 	return Person{fields[0], fields[1], fields[2], fields[3], dob}, nil
+}
+
+// Marshal converts a Person struct into a CSV row
+func Marshal(p Person) string {
+	year, month, day := p.DateOfBirth.Date()
+	return fmt.Sprintf("%s,%s,%s,%s,%02d/%02d/%d", p.LastName, p.FirstName, p.Gender, p.FavoriteColor, month, day, year)
+}
+
+// SortByGenderThenLastName sorts a slice of Person structs in place
+// oby gender then by last name.
+func SortByGenderThenLastName(persons []Person) {
+	sort.SliceStable(persons, func(i int, j int) bool {
+		if persons[i].Gender == persons[j].Gender {
+			return strings.ToLower(persons[i].LastName) < strings.ToLower(persons[j].LastName)
+		}
+		return persons[i].Gender < persons[j].Gender
+	})
 }
