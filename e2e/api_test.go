f5991a6fb2e971a3aeb19e22305e479ca000ceb0
Lucas Groenendaal <groenendaal92@gmail.com>
2019-01-15 05:00:41 +0000

    Setup API walking skeleton

---
+++ e2e/api_test.go
@@ -0,0 +1,35 @@
+// +build e2e
+
+package e2e_test
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestMain(m *testing.M) {
+	const numRetries = 10
+	for i := 0; i < numRetries; i++ {
+		if apiIsUp() {
+			exitCode := m.Run()
+			os.Exit(exitCode)
+		}
+		log.Print("could not connect to service, taking a short nap before retrying")
+		time.Sleep(3 * time.Second)
+	}
+	log.Fatal("timed out while waiting for service to start")
+}
+
+func apiIsUp() bool {
+	resp, err := http.Get(fmt.Sprintf("%s/healthcheck", os.Getenv("API_ENDPOINT")))
+	if err != nil {
+		fmt.Printf("when checking if the API was up we got an error which was: %v\n", err)
+		return false
+	}
+	resp.Body.Close()
+	return http.StatusOK == resp.StatusCode
+}
