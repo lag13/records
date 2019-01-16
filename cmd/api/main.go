54d4d1a8ca46d0a060849795f3f17c5bd256ed22
Lucas Groenendaal <groenendaal92@gmail.com>
2019-01-16 07:45:11 +0000

    Saving work
    I don't know if I'm rusty, if this problem is juuust different enough to
    be difficult, or if I'm trying too hard to make it perfect but I'm
    struggling a bit to organize this code in a way that I feel is very nice
    and maintainable.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"time"
+
+	"github.com/lag13/records/internal/multicsv"
+	"github.com/lag13/records/internal/person"
 )
+
+var db = []person.Person{}
 
 func main() {
 	mux := http.NewServeMux()
@@ -15,12 +23,74 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 	})
 	mux.HandleFunc("/records", func(w http.ResponseWriter, r *http.Request) {
+		// TODO: I wrote this code quickly to get a sense of
+		// what needs to happen and I plan to reorganize and
+		// unit test it. My problem though is that I don't
+		// like it when a units from the same repository
+		// reference eachother because if one unit breaks then
+		// the other will too. So, I'm not sure how to
+		// structure this code into a unit because it
+		// definitely needs to reference other units in this
+		// repository. Perhaps I make the unit accept two
+		// functions which encapsulate what we need from
+		// multicsv and person then in the real scenario we
+		// pass them in, but I'm not sure if I like that extra
+		// layer of abstraction. Or maybe we use the functions
+		// directly but only test for the presence of errors
+		// and not the exact wording (we will still test that
+		// the happy path transformation works as expected but
+		// I'm more okay with that since it probably won't
+		// change much if at all). Or maybe this stuff is so
+		// simple that covering it with e2e tests is
+		// sufficient?
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(`nopity nope`))
+			return
+		}
+		lines, parseErrs := multicsv.ReadAll("|, ", 5, r.Body)
+		if len(parseErrs) > 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(fmt.Sprintf("got some errors %s", parseErrs[0])))
+			return
+		}
+		p, parseErrs := person.Parse(lines[0])
+		if len(parseErrs) > 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(fmt.Sprintf("got some errors %s", strings.Join(parseErrs, ", "))))
+			return
+		}
+		db = append(db, p)
+		w.WriteHeader(http.StatusOK)
 	})
 	mux.HandleFunc("/records/gender", func(w http.ResponseWriter, r *http.Request) {
+		// TODO: Probably should copy the db here so this GET
+		// isn't modifying anything.
+		person.SortGenderLastNameAsc(db)
+		b, err := json.Marshal(db)
+		if err != nil {
+			panic(err)
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write(b)
 	})
 	mux.HandleFunc("/records/birthdate", func(w http.ResponseWriter, r *http.Request) {
+		person.SortBirthdateAsc(db)
+		b, err := json.Marshal(db)
+		if err != nil {
+			panic(err)
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write(b)
 	})
 	mux.HandleFunc("/records/name", func(w http.ResponseWriter, r *http.Request) {
+		person.SortLastNameDesc(db)
+		b, err := json.Marshal(db)
+		if err != nil {
+			panic(err)
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write(b)
 	})
 	srv := http.Server{
 		Addr:    ":8080",
--- e2e/api_test.go
+++ e2e/api_test.go
@@ -4,9 +4,12 @@ package e2e_test
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"testing"
 	"time"
 )
@@ -24,12 +27,83 @@ func TestMain(m *testing.M) {
 	log.Fatal("timed out while waiting for service to start")
 }
 
+var baseURL = "http://localhost:8080"
+
 func apiIsUp() bool {
-	resp, err := http.Get("http://localhost:8080/healthcheck")
+	resp, err := http.Get(fmt.Sprintf("%s/healthcheck", baseURL))
 	if err != nil {
 		fmt.Printf("when checking if the API was up we got an error which was: %v\n", err)
 		return false
 	}
 	resp.Body.Close()
 	return http.StatusOK == resp.StatusCode
+}
+
+func sendRequest(r *http.Request) *http.Response {
+	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		panic(err)
+	}
+	return resp
+}
+
+func newRequest(method string, path string, body io.Reader) *http.Request {
+	req, err := http.NewRequest(method, fmt.Sprintf("%s%s", baseURL, path), body)
+	if err != nil {
+		panic(err)
+	}
+	return req
+}
+
+func readAll(r io.Reader) []byte {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+func TestAPIFails(t *testing.T) {
+	resp := sendRequest(newRequest(http.MethodPost, "/records", strings.NewReader("hey,there buddy")))
+	if got, want := resp.StatusCode, http.StatusBadRequest; got != want {
+		t.Errorf("when posting invalid data got status code %d, want %d", got, want)
+	}
+	body := readAll(resp.Body)
+	// TODO: Is there even a point to testing that the error
+	// message is exactly what we want it to be? I'm not sure that
+	// there is. I feel like we should primarily worry about
+	// testing things which could be consumed by other programs.
+	// These error messages are meant for humans, not machines so
+	// if they vary I'm not sure it should matter. Testing that an
+	// error message exists seems important but I don't think we
+	// have to care about the specific contents.
+	if got, want := string(body), `{"error":"there should only be one type of separator in a single line but multiple separators (',', ' ') were specified"}`; got != want {
+		t.Errorf("when posting invalid data got body %s, want %s", got, want)
+	}
+}
+
+func TestAPISucceeds(t *testing.T) {
+	records := []string{
+		"Avatar,Aang,Male,Light-Orange,1760-12-13",
+		"MeatAndSarcasmGuy|Sokka|Male|Blue|1845-10-17",
+		"SoFullOfHope Katara Female Blue 1846-09-21",
+		"BlindBandit,Toph,Female,Green,1846-03-29",
+		"Lee|Zuko|Male|Red|1842-07-04",
+		"Crazy Azula Female Blood-Red 1842-12-30",
+		"Uncle,Iroh,Male,White,1820-08-24",
+	}
+	for _, record := range records {
+		resp := sendRequest(newRequest(http.MethodPost, "/records", strings.NewReader(record)))
+		if got, want := resp.StatusCode, http.StatusOK; got != want {
+			t.Errorf("when posting record %q got status code %d, want %d", record, got, want)
+		}
+	}
+	resp := sendRequest(newRequest(http.MethodGet, "/records/gender", nil))
+	if got, want := resp.StatusCode, http.StatusOK; got != want {
+		t.Errorf("when getting records sorted by gender, got status code %d, wanted %d", got, want)
+	}
+	body := readAll(resp.Body)
+	if got, want := string(body), `{"data":["BlindBandit,Toph,Female,Green,03/29/1846","Crazy,Azula,Female,Blood-Red,12/30/1842","SoFullOfHope,Katara,Female,Blue,09/21/1846","Avatar,Aang,Male,Light-Orange,12/13/1760","Lee,Zuko,Male,Red,07/04/1842","MeatAndSarcasmGuy,Sokka,Male,Blue,10/17/1845","Uncle,Iroh,Male,White,08/24/1820"]}`; got != want {
+		t.Errorf("when getting records sorted by gender, got response body %s, wanted %s", got, want)
+	}
 }
